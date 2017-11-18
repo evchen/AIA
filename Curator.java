3edaba3c36395790a1db7e6837a91e00f8dde21f
michpalm <michpalm95@gmail.com>
2017-11-18 18:34:45 +0100

    Behaviours added to Curator DF and Message

--- Curator.java
+++ Curator.java
@@ -3,15 +3,45 @@ package hw1;
 import jade.core.*;
 import jade.core.behaviours.*;
 import jade.lang.acl.*;
+import jade.domain.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.*;
 
 public class Curator extends Agent{
 	private String S;
 
 	protected void setup(){
+		addBehaviour(new RegisterOnDF(this));
+		addBehaviour(new ReceiveMessageBehaviour());
+		
 		Object[] args = getArguments();
 		if (args == null || args.length<= 0) return;
 		for (int i = 0; i<args.length; i++){
 			System.out.println("argument "+ (i+1) +" is " + (String)args[i]);
+		}
+	}
+	
+	private class RegisterOnDF extends OneShotBehaviour {
+		Agent a;
+		public RegisterOnDF(Agent a){
+			this.a = a;
+		}
+
+		public void action(){
+			DFAgentDescription dfd = new DFAgentDescription();
+			dfd.setName(getAID());
+			ServiceDescription sd = new ServiceDescription();
+			sd.setName("curator");
+			sd.setType("curator");
+			dfd.addServices(sd);
+
+			try{
+				DFService.register(a, dfd);
+			}
+			catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+			System.out.println(getAID()+" registered on DF.");
 		}
 	}
 
@@ -25,7 +55,7 @@ public class Curator extends Agent{
 			}
 		}
 		private void processTGMessage(String msg){
-			System.out.println("Curator " + getAID() + " got message" + msg);
+			System.out.println("Curator " + getAID() + " got message " + msg);
 		}
 
 	}
